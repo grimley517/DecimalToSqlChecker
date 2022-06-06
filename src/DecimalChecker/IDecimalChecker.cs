a3ac83b17f04ef0ab3024ca2a5595fb8851319b6
Brian.jones <ba.jones@me.com>
2022-06-06 21:30:57 +0100

    almost there

---
+++ src/DecimalChecker/IDecimalChecker.cs
@@ -0,0 +1,30 @@
+﻿namespace DecimalChecker
+{
+    public interface IDecimalChecker
+    {
+
+        int Scale { get; init; }
+        int Precision { get; init; }
+
+        /// <summary>
+        /// Gets the precision of a decimal number
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        int GetPrecision(decimal input);
+
+        /// <summary>
+        /// Gets the scale of a decimal number
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        byte GetScale(decimal input);
+
+        /// <summary>
+        /// Checks compatibility with a sql Server decimal data type
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        bool IsCompatible(decimal input);
+    }
+}
